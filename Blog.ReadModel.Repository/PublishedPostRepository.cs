995c7a399bc05236c53a12b7b524eecdc72f23de
Chris Day <chris.p.day@gmail.com>
2013-09-06 11:51:15 +0000

    Read most recent test pass
    git-tfs-id: [https://chrispday.visualstudio.com:443/DefaultCollection]$/Blog;C67

--- Blog.ReadModel.Repository/PublishedPostRepository.cs
+++ Blog.ReadModel.Repository/PublishedPostRepository.cs
@@ -10,6 +10,7 @@ namespace Blog.ReadModel.Repository
 {
 	public interface IPublishedPostRepository : IRepository<PublishedPost>
 	{
+		IEnumerable<PublishedPost> MostRecentPosts(int page, int pageSize, bool withOneMore);
 	}
 
 	internal class PublishedPostRepository : IPublishedPostRepository
@@ -18,7 +19,8 @@ namespace Blog.ReadModel.Repository
 
 		public void Save(PublishedPost item)
 		{
-			var entity = new DynamicTableEntity(item.Id.ToString(), "");
+			var order = (DateTime.MaxValue.ToUniversalTime() - item.WhenPublished.ToUniversalTime()).Ticks.ToString("D12");
+			var entity = new DynamicTableEntity(order + item.Id.ToString(), item.Id.ToString());
 			entity.Properties["WhenPublished"] = new EntityProperty(item.WhenPublished.ToUniversalTime().Ticks);
 			entity.Properties["Url"] = new EntityProperty(item.Url ?? "");
 			entity.Properties["Content"] = new EntityProperty(item.Content ?? "");
@@ -30,7 +32,7 @@ namespace Blog.ReadModel.Repository
 		{
 			return _table
 				.ExecuteQuery(new TableQuery<DynamicTableEntity>()
-				.Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, id.ToString())))
+				.Where(TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.Equal, id.ToString())))
 				.Select(entity => CreatePublishedPost(entity))
 				.FirstOrDefault();
 		}
@@ -38,6 +40,14 @@ namespace Blog.ReadModel.Repository
 		public IEnumerable<PublishedPost> Get()
 		{
 			return _table.ExecuteQuery(new TableQuery<DynamicTableEntity>())
+				.Select(entity => CreatePublishedPost(entity));
+		}
+
+		public IEnumerable<PublishedPost> MostRecentPosts(int page, int pageSize, bool withOneMore)
+		{
+			return _table.ExecuteQuery(new TableQuery<DynamicTableEntity>())
+				.Skip((page - 1) * pageSize)
+				.Take(pageSize + (withOneMore ? 1 : 0))
 				.Select(entity => CreatePublishedPost(entity));
 		}
 
@@ -50,7 +60,7 @@ namespace Blog.ReadModel.Repository
 		{
 			return new PublishedPost()
 			{ 
-				Id = new Guid(entity.PartitionKey), 
+				Id = new Guid(entity.RowKey), 
 				WhenPublished = new DateTime(entity.Properties["WhenPublished"].Int64Value.Value).ToLocalTime(),
 				Url = entity.Properties["Url"].StringValue,
 				Content = entity.Properties["Content"].StringValue
--- Blog.Tests/Reader/ReadMostRecentPosts.cs
+++ Blog.Tests/Reader/ReadMostRecentPosts.cs
@@ -1,6 +1,13 @@
 ﻿using System;
+using System.Linq;
 using StoryQ;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Blog.Tests;
+using Blog.Domain.Commands;
+using Blog.ReadModel.Repository;
+using System.Collections.Generic;
+using System.Threading;
+using Blog.ReadModel.Data;
 
 [TestClass]
 public class ReadMostRecentPosts_
@@ -21,23 +28,63 @@ public class ReadMostRecentPosts_
 			 .Execute();
 	}
 
+	Guid userId = Guid.NewGuid();
+	Guid sessionId;
 	private void SomePublishedPosts()
 	{
-		throw new NotImplementedException();
+		_.Receive(new CreateUser() { Id = userId, Password = userId.ToString(), Salt = userId, Username = userId.ToString() });
+		_.Receive(new Login() { Username = userId.ToString(), Password = userId.ToString() } );
+		sessionId = Repositories.Sessions.Get().Where(s => s.UserId == userId).Select(s => s.Id).First();
+
+		CreateAndPublishPost(Guid.NewGuid());
+		CreateAndPublishPost(Guid.NewGuid());
+		CreateAndPublishPost(Guid.NewGuid());
+		CreateAndPublishPost(Guid.NewGuid());
+		CreateAndPublishPost(Guid.NewGuid());
+		CreateAndPublishPost(Guid.NewGuid());
+		CreateAndPublishPost(Guid.NewGuid());
+		CreateAndPublishPost(Guid.NewGuid());
+		CreateAndPublishPost(Guid.NewGuid());
 	}
 
+	private void CreateAndPublishPost(Guid postId1)
+	{
+		_.Receive(new CreatePost() { Id = postId1, SessionId = sessionId, Title = postId1.ToString(), WhenCreated = DateTime.Now });
+		_.Receive(new EditPost() { Id = postId1, SessionId = sessionId, Content = postId1.ToString(), WhenEdited = DateTime.Now });
+		_.Receive(new PublishPost() { Id = postId1, SessionId = sessionId, WhenPublished = DateTime.Now });
+		Thread.Sleep(100);
+	}
+
+	List<IEnumerable<PublishedPost>> posts = new List<IEnumerable<PublishedPost>>();
+	int pagesize = 4;
 	private void TheMostRecentPostsAreAskedFor()
 	{
-		throw new NotImplementedException();
+		while (true)
+		{
+			var p = Repositories.PublishedPosts.MostRecentPosts(posts.Count + 1, pagesize, true).ToList();
+			posts.Add(p.Take(pagesize));
+			if (p.Count() < pagesize + 1)
+			{
+				break;
+			}
+		}
 	}
 
 	private void TheReaderCanViewPostsWithTheMostRecentFirst()
 	{
-		throw new NotImplementedException();
+		var all = posts.SelectMany(p => p.Select(sp => sp)).ToList();
+		for (int i = 0; i < all.Count - 1; i++)
+		{
+			Assert.IsTrue(all[i].WhenPublished >= all[i + 1].WhenPublished);
+		}
 	}
 
 	private void TheyArePaginated()
 	{
-		throw new NotImplementedException();
+		foreach (var p in posts.Take(posts.Count - 1))
+		{
+			Assert.AreEqual(pagesize, p.Count());
+		}
+		Assert.IsTrue(pagesize >= posts.Last().Count());
 	}
 }
