4767596d71529a76d9bae1493b26c7f07c98479a
Chris Day <chris.p.day@gmail.com>
2013-09-21 05:41:32 +0000

    layout
    git-tfs-id: [https://chrispday.visualstudio.com:443/DefaultCollection]$/Blog;C105

---
+++ Blog.Web.UI/Helper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Web.UI
+{
+	public static class Helper
+	{
+		public static string TwoLetterSuffix(this DateTime @this)
+		{
+			var dayMod10 = @this.Day % 10;
+
+			if (dayMod10 > 3 || dayMod10 == 0 || (@this.Day >= 10 && @this.Day <= 19))
+			{
+				return "\\t\\h";
+			}
+			else if (dayMod10 == 1)
+			{
+				return "\\s\\t";
+			}
+			else if (dayMod10 == 2)
+			{
+				return "n\\d";
+			}
+			else
+			{
+				return "r\\d";
+			}
+		}
+	}
+}
